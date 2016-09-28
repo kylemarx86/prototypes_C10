7f8ad64f1ae741f3eb7297540f09e3f997f8bb69
Kyle Marx <Kyle Marx>
2016-09-28 14:30:10 -0700

    PHP Sessions - Kyle Marx

--- php_sessions/session_reader.php
+++ php_sessions/session_reader.php
@@ -4,5 +4,8 @@
     $_SESSION['name'] = $_GET['name'];
     $_SESSION['age'] = $_GET['age'];
     $_SESSION['occupation'] = $_GET['occupation'];
-    print_r($_SESSION);
+//    print_r($_SESSION);
+
+    header('location: session_setter.php');
+
 ?>
