7d9f9e6db25db00597cc31a50c18a587bb824f59
Kyle Marx <Kyle Marx>
2016-10-02 02:21:45 -0700

    working on modal

--- php_directory_operations/gallery.php
+++ php_directory_operations/gallery.php
@@ -13,7 +13,7 @@
                 success: function (response) {
                     if(response.success){
                         var images = response.files;
-                        var $container = $('<div>');
+                        var $container = $('<div>').addClass('container').attr('id','imageContainer');
                         for (var i = 0; i < images.length; i++) {
                             var $image = $('<img>').attr({
                                 src: images[i],
@@ -31,15 +31,54 @@
             });
         }
 
+        function createBlankModal() {
+            //create div with class modal and fade with id = imageModal
+            var $modal = $('<div>').addClass('modal fade').attr('id','imageModal');
+            //create div with class modal-dialog
+            var $modalDialog = $('<div>').addClass('modal-dialog');
+            //create div with class modal-content
+            var $modalContent = $('<div>').addClass('modal-content');
+            //create image
+//            var $image = $('<img>').attr('src','images/beagle_.jpg');   //temp hardcoded
+//            var $image = $('<img>').attr('src',$(this).attr('src'));      //is prob gonna work
+            var $image = $('<img>');
+            //add image to div with class modal-content
+            $modalContent.append($image);
+            //add div with class modal-content to div with class modal-dialog
+            $modalDialog.append($modalDialog);
+            //add div with class modal-dialog to div with class modal
+            $modal.append($modalDialog);
+            //add div with class modal to the container
+            $('#image_container').append($modal);
+        }
+
         function handleImageClick() {
-            console.log('image was clicked');
-            var $modal = $('<div>').addClass('modal fade').attr('role','dialog');
-            var $image = $('<img>').attr('src',$(this).attr('src'));
-            $image.attr('width','100%');
-//            console.log($image);
-            $modal.append($image);
-            console.log('this is ',this);
-            $('body').append($modal);
+            $(this).attr('data-toggle','modal');
+            //change image source in the modal
+
+            //show the modal
+
+
+            console.log('this this right now: ', this);
+            console.log('this image right now: ', $($image));
+
+//            $(this).attr({'data-toggle':'modal','data-target':$modal});
+
+
+
+//            var $container = $('<div>').addClass('container');
+//            var $modal = $('<div>').addClass('modal fade').attr('role','dialog');
+//            var $modal_content = $('<div>').addClass('modal-content');
+//            var $image = $('<img>').attr('src','images/beagle_.jpg');
+////            var $image = $('<img>').attr('src',$(this).attr('src'));      //is prob gonna work
+//            $image.attr('width','100%');
+//            $modal_content.append($image);
+//            $modal.append($modal_content);
+//
+//            $modal.append('#imageContainer');
+////            $('body').append($container);
+//            console.log('this is ',this);
+//            $(this).attr({'data-toggle':'modal','data-target':$modal});
         }
 
         load_files();
