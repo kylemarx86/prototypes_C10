5ff99d16800f5063451f61fd6213164bb90d3aa8
Kyle Marx <Kyle Marx>
2016-10-03 19:42:13 -0700

    pictures move now

--- php_directory_operations/carousel.php
+++ php_directory_operations/carousel.php
@@ -8,26 +8,18 @@
     <script src="https://code.jquery.com/jquery-2.1.4.js"></script>
     <script>
         var image_array = [];
-        var current_image = null;
+        var current_image_index = null;
 
         $(document).ready(function () {
             createFeaturesAndButtons();
             load_files();
-            applyEventHandlers();
+
 
         });
 
         function createFeaturesAndButtons() {
-            var $container = $('<div>').attr('id','image_container');
-            //create next and previous buttons and attach the appropriate handler
-            var $prevButton = $('<button>').attr('id','prevButton').text('<');
-            var $nextButton = $('<button>').attr('id','nextButton').text('>');
 
-            //add buttons to container
-            $container.append($prevButton);
-            $container.append($nextButton);
-            //add container to body
-            $('body').append($container);
+
         }
         //make ajax call to get_images.php and saves those images to image_array
         function load_files() {
@@ -36,8 +28,15 @@
                 dataType: 'json',
                 success: function (response) {
                     if(response.success){
+
+                        var $container = $('<div>').attr('id','image_container');
+                        //add container to body
+                        $('body').append($container);
+
                         //gather all images
                         var files = response.files;
+
+
 
                         //set up the gathered images
                         for(var i = 0; i < files.length; i++){
@@ -46,6 +45,15 @@
 //                            var $image = $('<img>').attr('src',image_array[i]);
                         }
                         initialize();
+
+                        //create next and previous buttons and attach the appropriate handler
+                        var $prevButton = $('<button>').attr('id','prevButton').text('<');
+                        var $nextButton = $('<button>').attr('id','nextButton').text('>');
+
+                        //add buttons to container
+                        $container.append($prevButton);
+                        $container.append($nextButton);
+                        applyEventHandlers();
 
                         //create all images
 
@@ -63,68 +71,63 @@
         //sets up pictures for display
         function initialize() {
             //create an image and set the source
-            current_image = 0;
+            current_image_index = 0;
 
-            for(var i = 0; i < image_array.length; i++){
+            for(var i = 1; i < image_array.length; i++){
 //                image_array[i].attr({'left':'100%','visibility':'hidden'});
-                image_array[i].attr('left','100%');
+                image_array[i].css('left','100%');
             }
-//            image_array[current_image].attr('visibility','visible');
+            image_array[0].css('left','0%');
+
+//            current_image_index = 0;
+//            console.log('current_image_index ', current_image_index);
+//            image_array[current_image_index].attr('visibility','visible');
         }
         
         function next_image(){
-//            console.log('next image');
-            if(current_image < image_array.length - 1){
-                current_image += 1;
-            }else{
-                current_image = 0;
-            }
-            update_image();
+            console.log('next image');
+            update_image(1);
         }
         
         function prev_image() {
-//            console.log('previous image');
-            if(current_image > 0){
-                current_image -= 1;
-            }else{
-                current_image = image_array.length - 1;
-            }
-            update_image();
+            console.log('previous image');
+            update_image(-1);
         }
 
-        function update_image(direciton) {
-//            image_array
-//            $('#image_container').find('img').attr('src',image_array[current_image]);
-
-
+        //takes param direction,
+        //if direction = 1, then we will move forward through the image array (i.e. increase index)
+        //if direction = -1, then we will move backward through the image array (i.e. decrease index)
+        function update_image(direction) {
             //declare old image
 
             //declare new image
             var new_image_index = null;
-            if(direciton === 1){
-                if (current_image < image_array.length - 1) {
-                    new_image_index += 1;
+            if(direction === 1){
+                if (current_image_index < image_array.length - 1) {
+                    new_image_index = current_image_index + 1;
                 } else {
                     new_image_index = 0;
                 }
             }else{
-                if(current_image > 0){
-                    new_image_index -= 1;
+                if(current_image_index > 0){
+                    new_image_index = current_image_index - 1;
                 }else{
                     new_image_index = image_array.length - 1;
                 }
             }
 
-
             //prepare new image for move in
 
             //slide previous image out
+            $(image_array[current_image_index]).animate({left: 100*direction+'%'},3000);
 
             //slide new image in
-//            image_array[current_image].visibility('visible');
+            $(image_array[new_image_index]).animate({left: 100*direction+'%'},3000);
 
-            //update current_image
-            current_image = new_image_index;
+            //update current_image_index
+            current_image_index = new_image_index;
+
+            console.log('current_image_index: ', current_image_index);
         }
         
         function applyEventHandlers() {
