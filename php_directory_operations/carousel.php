e609de1ff7bfb9d5f43fe4309bb6dd366c764e2a
Kyle Marx <Kyle Marx>
2016-10-04 20:23:07 -0700

    carousel working. wooooohooo. should add delays to buttons to prevent from clicking too quickly

--- php_directory_operations/carousel.php
+++ php_directory_operations/carousel.php
@@ -11,16 +11,9 @@
         var current_image_index = null;
 
         $(document).ready(function () {
-            createFeaturesAndButtons();
             load_files();
-
-
         });
 
-        function createFeaturesAndButtons() {
-
-
-        }
         //make ajax call to get_images.php and saves those images to image_array
         function load_files() {
             $.ajax({
@@ -28,39 +21,28 @@
                 dataType: 'json',
                 success: function (response) {
                     if(response.success){
-
+                        //create container to place images and buttons in
                         var $container = $('<div>').attr('id','image_container');
                         //add container to body
                         $('body').append($container);
-
                         //gather all images
                         var files = response.files;
-
-
-
                         //set up the gathered images
                         for(var i = 0; i < files.length; i++){
                             image_array.push($('<img>').attr('src', files[i]));
                             $('#image_container').append(image_array[i]);
-//                            var $image = $('<img>').attr('src',image_array[i]);
                         }
+                        //initialize pictures
                         initialize();
-
                         //create next and previous buttons and attach the appropriate handler
                         var $prevButton = $('<button>').attr('id','prevButton').text('<');
                         var $nextButton = $('<button>').attr('id','nextButton').text('>');
-
                         //add buttons to container
                         $container.append($prevButton);
                         $container.append($nextButton);
+                        //add event handlers to buttons
                         applyEventHandlers();
-
-                        //create all images
-
-                        //stack all images
-
                     }
-
                 },
                 error: function (response) {
                     console.log('connection error');
@@ -74,23 +56,18 @@
             current_image_index = 0;
 
             for(var i = 1; i < image_array.length; i++){
-//                image_array[i].attr({'left':'100%','visibility':'hidden'});
                 image_array[i].css('left','100%');
             }
             image_array[0].css('left','0%');
-
-//            current_image_index = 0;
-//            console.log('current_image_index ', current_image_index);
-//            image_array[current_image_index].attr('visibility','visible');
         }
         
         function next_image(){
-            console.log('next image');
+//            console.log('next image');
             update_image(1);
         }
         
         function prev_image() {
-            console.log('previous image');
+//            console.log('previous image');
             update_image(-1);
         }
 
@@ -98,8 +75,6 @@
         //if direction = 1, then we will move forward through the image array (i.e. increase index)
         //if direction = -1, then we will move backward through the image array (i.e. decrease index)
         function update_image(direction) {
-            //declare old image
-
             //declare new image
             var new_image_index = null;
             if(direction === 1){
@@ -108,18 +83,15 @@
                 } else {
                     new_image_index = 0;
                 }
-                //prepare new image for move in
-                $(image_array[new_image_index]).css('left', '100%');
             }else{
                 if(current_image_index > 0){
                     new_image_index = current_image_index - 1;
                 }else{
                     new_image_index = image_array.length - 1;
                 }
-                //prepare new image for move in
-                $(image_array[new_image_index]).css('right', '100%');
             }
-
+            //prepare new image for move in
+            $(image_array[new_image_index]).css('left', direction*100+'%');
             //slide previous image out
             $(image_array[current_image_index]).animate({left: -100*direction+'%'},3000);
             //slide new image in
@@ -127,7 +99,7 @@
             //update current_image_index
             current_image_index = new_image_index;
 
-            console.log('current_image_index: ', current_image_index);
+//            console.log('current_image_index: ', current_image_index);
         }
         
         function applyEventHandlers() {
